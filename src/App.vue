f94adf23d75a05169be331ae267d0bf4f886f6b7
faiz.hasnul <faiz.hasnul@paynet.my>
2024-04-05 15:53:47 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div class="min-h-screen overflow-hidden">
+    <RouterView />
+  </div>
+</template>
+
+<style scoped></style>
