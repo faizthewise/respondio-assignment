27930ee9e5f70805e357618414167b1aae081c98
faiz.hasnul <faiz.hasnul@paynet.my>
2024-04-14 14:42:32 +0800

    Feat - ConnectorNode

---
+++ src/components/main/ConnectorNode.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import type { NodeProps } from "@vue-flow/core";
+
+import type { NodeCustomData } from "@/ts/interface";
+
+defineProps<NodeProps<NodeCustomData>>();
+</script>
+
+<template>
+  <div class="border rounded-lg bg-primary-low">
+    <div class="flex items-center gap-2 px-8 py-2">
+      <p class="font-medium text-black">{{ data.title }}</p>
+    </div>
+  </div>
+</template>
